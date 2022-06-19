36d39e313846d1f2fb42eb2e096ea7c06dfee300
Pulkit Singh <pulkitsingh01@gmail.com>
2022-06-19 14:56:02 +0530

    argon2 hash password matching

--- pages/api/first-factor.ts
+++ pages/api/first-factor.ts
@@ -2,10 +2,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { FirstFactorRequestArgs } from '../../types/first-factor.types';
 import db_connection from '../../utils/db';
+import argon2 from 'argon2';
 
 type Data = {
-  name: string
-}
+  name: string;
+} | {
+  errorMessage?: string;
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -15,24 +18,34 @@ export default async function handler(
   console.log({ cookies: req.cookies });
 
   try {
-    console.log("req nom", req.body)
-    const query = 'SELECT * from auth_user'
-    const result = await db_connection.query(
-      query
-    );
-    console.log("ttt", result.rows);
-  } catch (error) {
-    console.log(error);
+    // console.log("req nom", req.body);
+    const query = `SELECT * from auth_user 
+        WHERE username=lower('${username}')`;
+        // AND password='${await argon2.hash(password)}'`;
+    // console.log({ query });
+    const { rows } = await db_connection.query(query);
+    
+    if (!rows.length) {
+      throw new Error('Empty record!');
+    }
+
+    const record = rows[0];
+    if (!await argon2.verify(record.password, password)) {
+      throw new Error('Wrong password!');
+    }
+
+    res
+      // .setHeader('Access-Control-Allow-Origin', '*')
+      // .setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+      // .setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,set-cookie')
+      // .setHeader('Access-Control-Allow-Credentials', 'true')
+      // .setHeader('Access-Control-Expose-Headers', 'Set-Cookie')
+      .setHeader('Set-Cookie', `authToken=abcd; HttpOnly; secure;`)
+      .status(200)
+      .json(record);
+    // res.status(401).end();
+
+  } catch (errorMessage: any) {
+    res.status(401).send({ errorMessage });
   }
-  
-  res
-    // .setHeader('Access-Control-Allow-Origin', '*')
-    // .setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    // .setHeader('Access-Control-Allow-Headers', 'Origin,X-Requested-With,content-type,set-cookie')
-    // .setHeader('Access-Control-Allow-Credentials', 'true')
-    // .setHeader('Access-Control-Expose-Headers', 'Set-Cookie')
-    .setHeader('Set-Cookie', `authToken=abcd; HttpOnly; secure;`)
-    .status(200)
-    .json(req.body);
-  // res.status(401).end();
 }
